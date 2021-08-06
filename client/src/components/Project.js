85e2ee567b34b0e7510f2418a4bf644a9a49187a
Yashvardhan Arora <yash22arora@gmail.com>
2021-08-07 03:09:47 +0530

    Add project section

---
+++ client/src/components/Project.js
@@ -0,0 +1,11 @@
+import "../assets/css/Projects.css";
+
+const Project=()=>{
+    return(
+        <div className="project">
+            
+        </div>
+    );
+}
+
+export default Project;
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,10 +1,21 @@
 import "../assets/css/Projects.css";
-
+import Project from "./Project";
 const Projects =()=>
 {
     return(
         <div className="projects" id="projects">
-           
+           <div className="projects-heading">
+                <p>-FEATURED PROJECTS</p>
+                <h1>Things I've worked on 👨🏼‍💻</h1>
+           </div>
+           <div className="project-list">
+                <Project/>
+                <Project/>
+                <Project/>
+                <Project/>
+                <Project/>
+
+           </div>
         </div>
     )
 }
