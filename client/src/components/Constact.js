57b02a3825e84e7c792ea03b3ec3c128cf8e7771
Yashvardhan Arora <yash22arora@gmail.com>
2021-08-09 02:39:26 +0530

    Add contact info and hindi

--- client/src/components/Constact.js
+++ client/src/components/Constact.js
@@ -4,12 +4,14 @@ const ContactMe=()=>
 {
     return(
         <div className="social" id="social">
+            <span className="ya">यश</span> 
            <div className="social-heading">
                 <p>-GOT SOME WORK FOR ME?</p>
                 <h1>Contact Me here 📫</h1>
            </div>
            <div className="contact-info">
-               
+               <a href="tel: +91 9625056985">📞 : +91 96250 56985</a>
+               <a href="mailto: yashvardhan.arora@outlook.com">✉️ : yashvardhan.arora@outlook.com</a>
            </div>
         </div>
     )
