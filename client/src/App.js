8ba40d00217c381ffe800ce8594e8da5189878ec
Yashvardhan Arora <yash22arora@gmail.com>
2022-09-22 13:50:06 +0530

    Dynamic title, emoji-explosion on high-five
    Signed-off-by: Yashvardhan Arora <yash22arora@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,87 @@
-
-import './App.css';
-import ContactMe from './components/Constact';
-import Landing from './components/Landing';
-import Projects from './components/Projects';
-import SocialMedia from './components/SocialMedia';
-import VisitorCount from './components/VisitorCount';
+import "./App.css";
+import ContactMe from "./components/Contact";
+import Landing from "./components/Landing";
+import Projects from "./components/Projects";
+import SocialMedia from "./components/SocialMedia";
+import VisitorCount from "./components/VisitorCount";
 
 function App() {
-  
   const projects = [
     {
       title: "Podcasts Page",
       body: "Designed and coded the Podcasts page for the DSC official website from scratch. Integrated the Anchor player to play any podcast directly from the page.",
       techStack: ["React", "API", "CSS"],
-      github: "https://github.com/developer-student-club-thapar/officialWebsite/tree/master/client/src/pages/Podcasts",
+      github:
+        "https://github.com/developer-student-club-thapar/officialWebsite/tree/master/client/src/pages/Podcasts",
       external: "https://dsctiet.tech/#/podcast",
-
     },
     {
-      title:"30 Days GCP Leaderboard", 
-      body:"Made a Google themed website showing leaderboard for the students of Thapar participating in the 30 days of Google Cloud Challenge, with graphs and tables and much more!",
+      title: "30 Days GCP Leaderboard",
+      body: "Made a Google themed website showing leaderboard for the students of Thapar participating in the 30 days of Google Cloud Challenge, with graphs and tables and much more!",
       techStack: ["React", "ChartJs 2.0", "API"],
-      github:"https://github.com/yash22arora/30DaysOfCloudLeaderBoard",
-      external:"https://30daysofgcp.dsctiet.tech/"
+      github: "https://github.com/yash22arora/30DaysOfCloudLeaderBoard",
+      external: "https://30daysofgcp.dsctiet.tech/",
     },
     {
-      title:"Notefy", 
-      body:"A note-making webapp with an elegant and intuitive UI",
+      title: "Notefy",
+      body: "A note-making webapp with an elegant and intuitive UI",
       techStack: ["React", "API", "Django"],
-      github:"https://github.com/yash22arora/notefy",
-      external:"https://notefy.servatom.com/"
+      github: "https://github.com/yash22arora/notefy",
+      external: "https://notefy.servatom.com/",
     },
     {
-      title:"Amazon UX Case Study",
-      body:"Recognising pain points in the Amazon shopping app",
+      title: "Amazon UX Case Study",
+      body: "Recognising pain points in the Amazon shopping app",
       techStack: ["Figma", "UI/UX"],
-      behance:"https://www.behance.net/gallery/125089737/Amazon-App-UX-Case-Study",
-      external:""
+      behance:
+        "https://www.behance.net/gallery/125089737/Amazon-App-UX-Case-Study",
+      external: "",
     },
     {
-      title:"Servatom",
-      body:"The website for Servatom, an organisation of enthusiastic techies.",
+      title: "Servatom",
+      body: "The website for Servatom, an organisation of enthusiastic techies.",
       techStack: ["React", "Github API"],
-      github:"https://github.com/Servatom",
-      external:"https://servatom.com/"
+      github: "https://github.com/Servatom",
+      external: "https://servatom.com/",
     },
     {
-      title:"CTF Portal",
-      body:"The online web portal to host a CTF event for my society",
+      title: "CTF Portal",
+      body: "The online web portal to host a CTF event for my society",
       techStack: ["React", "MongoDB", "NodeJs"],
-      github:"https://github.com/OWASP-STUDENT-CHAPTER/CTF",
-      external:"https://drive.google.com/drive/folders/1GPvQykH30aIUUadY-kLmG-4XRcKDlarB?usp=sharing"
+      github: "https://github.com/OWASP-STUDENT-CHAPTER/CTF",
+      external:
+        "https://drive.google.com/drive/folders/1GPvQykH30aIUUadY-kLmG-4XRcKDlarB?usp=sharing",
     },
     {
-      title:"Shealth", 
-      body:"An online solution to organise your medical history and give your doctors secured access to your records",
+      title: "Shealth",
+      body: "An online solution to organise your medical history and give your doctors secured access to your records",
       techStack: ["React", "API", "Django"],
-      github:"https://github.com/yash22arora/Shealth/",
-      external:"https://shealth.servatom.com/"
+      github: "https://github.com/yash22arora/Shealth/",
+      external: "https://shealth.servatom.com/",
     },
     {
-      title:"Second Hand",
-      body:"An online platform for buying and selling used objects within college",
+      title: "Second Hand",
+      body: "An online platform for buying and selling used objects within college",
       techStack: ["HTML", "CSS", "Python"],
-      github:"https://github.com/Servatom/Second-Hand",
-      external:""
+      github: "https://github.com/Servatom/Second-Hand",
+      external: "",
     },
   ];
 
+  document.addEventListener("visibilitychange", (e) => {
+    if (document.visibilityState === "visible") {
+      document.title = "Yashvardhan Arora";
+    } else {
+      document.title = "👀 Come back soon!";
+    }
+  });
 
-  
   return (
     <div className="App">
-      <Landing/>
-      <Projects projects={projects}/>
-      <SocialMedia/>
-      <ContactMe/>
+      <Landing />
+      <Projects projects={projects} />
+      <SocialMedia />
+      <ContactMe />
       {/* <VisitorCount/> */}
     </div>
   );
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,16 +1,76 @@
 import "../assets/css/Landing.css";
-const Landing =()=>
-{
-    return(
-        <div className="canvas">
-            <div className="header-text">
-                <p>HI THERE 👋, I'M</p>
-                <h1>Yashvardhan Arora</h1>
-                <p className="tagline">A budding Design-Minded Front-end Developer 💻</p>
-            </div>
-            {/* <iframe className="spline" src='https://my.spline.design/lathelamps-d7eb10a1026df83a37190114418298e5/' frameborder='0' width='100%' height='100%'></iframe> */}
-        </div>
-    );
-}
+import { emojisplosion, emojisplosions } from "emojisplosion";
+const Landing = () => {
+  const element = document.getElementById("highfive");
+  console.log(element);
+
+  const cumulativeOffset = function (element) {
+    let top = 0,
+      left = 0;
+    do {
+      top += element?.offsetTop || 0;
+      left += element?.offsetLeft || 0;
+      element = element?.offsetParent;
+    } while (element);
+    console.log(top, left);
+    return {
+      top: top,
+      left: left,
+    };
+  };
+  const onHover = () => {
+    emojisplosion({
+      className: "emojiExplosion",
+      emojis: ["👋", "🤙", "🙌"],
+      physics: {
+        gravity: 0.6,
+        initialVelocities: {
+          y: {
+            min: -25,
+            max: -10,
+          },
+          x: {
+            min: -10,
+            max: 10,
+          },
+        },
+        fontSize: {
+          min: 20,
+          max: 70,
+        },
+      },
+      position: {
+        x: 0,
+        y: cumulativeOffset(element).top,
+        // y: 0,
+      },
+    });
+  };
+
+  document.addEventListener("keypress", (e) => {
+    if (e.key.toLowerCase() === "y") {
+      onHover();
+    }
+  });
+
+  return (
+    <div className="canvas">
+      <div className="header-text">
+        <p>
+          HI THERE{" "}
+          <span id="highfive" onMouseEnter={onHover}>
+            👋
+          </span>
+          , I'M
+        </p>
+        <h1 id="yashvardhan">Yashvardhan Arora</h1>
+        <p className="tagline">
+          A budding Design-Minded Front-end Developer 💻
+        </p>
+      </div>
+      {/* <iframe className="spline" src='https://my.spline.design/lathelamps-d7eb10a1026df83a37190114418298e5/' frameborder='0' width='100%' height='100%'></iframe> */}
+    </div>
+  );
+};
 
 export default Landing;
